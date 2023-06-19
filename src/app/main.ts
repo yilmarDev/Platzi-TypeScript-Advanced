aaf8f475cee54604b7cc8b64df44e0e82bde294a
Yilmar Lopez <ospina.yilmar@gmail.com>
2023-06-19 16:30:19 -0500

    Libreria Faker instalada Generación automática de productos implementada con Faker Lección 16

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,16 +1,27 @@
+import { faker } from '@faker-js/faker';
 import { products, addProduct } from './products/product.service';
-import {} from './categories/category.model';
 
-addProduct({
-  id: 45,
-  title: 'Camisa a rayas',
-  createdAt: new Date(),
-  updatedAt: new Date(),
-  stock: 100,
-  category: {
-    id: 78,
-    name: 'ropa para caballeros',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-});
+for (let index = 0; index < 50; index++) {
+  addProduct({
+    id: faker.string.uuid(),
+    description: faker.commerce.productDescription(),
+    title: faker.commerce.productName(),
+    image: faker.image.url(),
+    stock: faker.number.int({ min: 10, max: 100 }),
+    size: faker.helpers.arrayElement(['S', 'M', 'L', 'XL']),
+    color: faker.color.human(),
+    price: parseInt(faker.commerce.price(), 10),
+    isNew: faker.datatype.boolean(),
+    tags: faker.helpers.arrayElements(['new', 'white', 'tshirt', 'clothes']),
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+    category: {
+      id: faker.datatype.uuid.toString(),
+      name: faker.commerce.department(),
+      createdAt: faker.date.recent(),
+      updatedAt: faker.date.recent(),
+    },
+  });
+}
+
+console.log(products);
--- src/app/products/product.model.ts
+++ src/app/products/product.model.ts
@@ -5,7 +5,13 @@ export type Sizes = 'S' | 'M' | 'L' | 'XL';
 
 export interface Product extends BaseModel {
   title: string;
+  description: string;
+  image: string;
   stock: number;
   size?: Sizes;
+  color: string;
+  price: number;
+  isNew: boolean;
+  tags: string[];
   category: Category;
 }
